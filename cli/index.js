ac87d054755ff64f68bd9b97f87ea87d25105a39
James Dalton <tarpdalton@users.noreply.github.com>
2021-07-09 18:03:41 -0700

    switch to node webcrypto

--- cli/index.js
+++ cli/index.js
@@ -2,23 +2,13 @@
 
 'use strict';
 
-var CryptoJS = require("crypto-js");
-var FileSystem = require("fs");
-const path = require("path");
-const Yargs = require('yargs');
+const { subtle, getRandomValues } = require('crypto').webcrypto;
+const { readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
+const { usage, showHelp } = require('yargs');
 
-const SCRIPT_URL = 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js';
-const SCRIPT_TAG = '<script src="' + SCRIPT_URL + '" integrity="sha384-lp4k1VRKPU9eBnPePjnJ9M2RF3i7PC30gXs70+elCVfgwLwx1tv5+ctxdtwxqZa7" crossorigin="anonymous"></script>';
-
-const namedArgs = Yargs
-      .usage('Usage: staticrypt <filename> <passphrase> [options]')
+const namedArgs = usage('Usage: staticrypt <filename> <passphrase> [options]')
       .demandCommand(2)
-      .option('e', {
-          alias: 'embed',
-          type: 'boolean',
-          describe: 'Whether or not to embed crypto-js in the page (or use an external CDN)',
-          default: true
-      })
       .option('o', {
           alias: 'output',
           type: 'string',
@@ -41,12 +31,12 @@ const namedArgs = Yargs
           alias: 'file-template',
           type: 'string',
           describe: 'Path to custom HTML template with password prompt.',
-          default: path.join(__dirname, 'password_template.html')
+          default: join(__dirname, 'password_template.html')
       })
       .argv;
 
 if(namedArgs._.length !== 2){
-    Yargs.showHelp();
+    showHelp();
     process.exit(1);
 }
 
@@ -54,69 +44,51 @@ const input = namedArgs._[0].toString();
 const password = namedArgs._[1].toString();
 
 try{
-    var contents = FileSystem.readFileSync(input, 'utf8');
+    var contents = readFileSync(input, 'utf8');
 }catch(e){
     console.log("Failure: input file does not exist!");
     process.exit(1);
 }
 
-/**
- * Salt and encrypt a msg with a password.
- * Inspired by https://github.com/adonespitogo
- */
-var keySize = 256;
-var iterations = 1000;
 function encrypt (msg, password) {
-    var salt = CryptoJS.lib.WordArray.random(128/8);
+    var iv = getRandomValues(new Uint8Array(16));
+    var ivHex = bytesToHexString(iv);
+    var pwUtf8 = stringToUint8Array(password);
 
-    var key = CryptoJS.PBKDF2(password, salt, {
-        keySize: keySize/32,
-        iterations: iterations
-    });
-
-    var iv = CryptoJS.lib.WordArray.random(128/8);
-
-    var encrypted = CryptoJS.AES.encrypt(msg, key, {
-        iv: iv,
-        padding: CryptoJS.pad.Pkcs7,
-        mode: CryptoJS.mode.CBC
-    });
-
-    // salt, iv will be hex 32 in length
-    // append them to the ciphertext for use  in decryption
-    var encryptedMsg = salt.toString()+ iv.toString() + encrypted.toString();
-    return encryptedMsg;
+    subtle.digest('SHA-256', pwUtf8)
+    .then(function(hash) {
+        return bytesToHexString(hash);
+    })
+    .then(function(pwHex) {
+    return hexStringToUint8Array(pwHex);
+    })
+    .then(function(keyData) {
+    return subtle.importKey("raw", keyData, "AES-GCM", false, ["encrypt"]);
+    })
+    .then(function(key) {
+        return subtle.encrypt({
+            name: "AES-GCM",
+            iv: iv
+        }, key, stringToUint8Array(msg));
+    })
+    .then(function(cipherText) {
+        return ivHex + bytesToHexString(cipherText);
+    })
+    .then(function(encrypted) {
+        return {
+            title: namedArgs.title,
+            instructions: namedArgs.instructions,
+            encrypted: encrypted,
+            outputFilePath: namedArgs.output !== null ? namedArgs.output : input.replace(/\.html$/, '') + "_encrypted.html"
+        };
+    })
+    .then(function(data) {
+        genFile(data);
+    })
+    .catch(console.log);
 }
 
-// encrypt input
-var encrypted = encrypt(contents, password);
-var hmac = CryptoJS.HmacSHA256(encrypted, CryptoJS.SHA256(password).toString()).toString();
-var encryptedMessage = hmac + encrypted;
-
-// create crypto-js tag (embedded or not)
-var cryptoTag = SCRIPT_TAG;
-if (namedArgs.embed) {
-    try {
-        var embedContents = FileSystem.readFileSync(path.join(__dirname, 'crypto-js.min.js'), 'utf8');
-    } catch(e) {
-        console.log("Failure: embed file does not exist!");
-        process.exit(1);
-    }
-    cryptoTag = '<script>' + embedContents + '</script>';
-}
-
-
-var data = {
-    title: namedArgs.title,
-    instructions: namedArgs.instructions,
-    encrypted: encryptedMessage,
-    crypto_tag: cryptoTag,
-    embed: namedArgs.embed,
-    outputFilePath: namedArgs.output !== null ? namedArgs.output : input.replace(/\.html$/, '') + "_encrypted.html"
-};
-
-genFile(data);
-
+encrypt(contents, password)
 
 /**
  * Fill the template with provided data and writes it to output file.
@@ -125,7 +97,7 @@ genFile(data);
  */
 function genFile(data){
     try{
-        var templateContents = FileSystem.readFileSync(namedArgs.f, 'utf8');
+        var templateContents = readFileSync(namedArgs.f, 'utf8');
     }catch(e){
         console.log("Failure: could not read template!");
         process.exit(1);
@@ -134,7 +106,7 @@ function genFile(data){
     var renderedTemplate = render(templateContents, data);
 
     try{
-        FileSystem.writeFileSync(data.outputFilePath, renderedTemplate);
+        writeFileSync(data.outputFilePath, renderedTemplate);
     }catch(e){
         console.log("Failure: could not generate output file!");
         process.exit(1);
@@ -152,4 +124,44 @@ function render(tpl, data){
     return tpl.replace(/{(.*?)}/g, function (_, key) {
         return data && data[key] || '';
     });
+}
+
+/*
+* encode/decode funtions
+*/
+
+function hexStringToUint8Array(hexString) {
+    if (hexString.length % 2 != 0)
+        throw "Invalid hexString";
+    var arrayBuffer = new Uint8Array(hexString.length / 2);
+
+    for (var i = 0; i < hexString.length; i += 2) {
+        var byteValue = parseInt(hexString.substr(i, 2), 16);
+        if (byteValue == NaN)
+            throw "Invalid hexString";
+        arrayBuffer[i / 2] = byteValue;
+    }
+
+    return arrayBuffer;
+}
+
+function bytesToHexString(bytes) {
+    if (!bytes)
+        return null;
+
+    bytes = new Uint8Array(bytes);
+    var hexBytes = [];
+
+    for (var i = 0; i < bytes.length; ++i) {
+        var byteString = bytes[i].toString(16);
+        if (byteString.length < 2)
+            byteString = "0" + byteString;
+        hexBytes.push(byteString);
+    }
+    return hexBytes.join("");
+}
+
+function stringToUint8Array(str) {
+    var encoder = new TextEncoder('utf-8');
+    return encoder.encode(str);
 }
