b2139f3930cd063acd381dfaa888c3d5146bbac7
Adam Hull <adam@hmlad.com>
2022-10-23 13:04:58 -0700

    organize into directories

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,10 @@
 # Usage: `npm run build`
 # NPM establishes a reliable working directory
 
-cd ..
-# Copy files that should be included in `npm publish`
-cp README.md LICENSE password_template.html cli/
 # Encrypt the example file
-npx ./cli example.html test \
+npx . example/example.html test \
     --no-embed \
     --salt b93bbaf35459951c47721d1f3eaeb5b9 \
     --instructions "Enter \"test\" to unlock the page"
+
+cp lib/password_template.html www/
