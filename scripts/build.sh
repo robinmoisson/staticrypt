225d7b25172231de14dfcf886ac8fc230239bf98
Adam Hull <adam@hmlad.com>
2022-10-23 16:23:37 -0700

    dedupe common crypto functions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,21 @@
 # Usage: `npm run build`
 # NPM establishes a reliable working directory
 
-# Encrypt the example file
+#
+# Example
+#
 npx . example/example.html test \
     --no-embed \
     --salt b93bbaf35459951c47721d1f3eaeb5b9 \
     --instructions "Enter \"test\" to unlock the page"
+
+#
+# WWW
+#
+# Inline www dependencies using staticrypt's internal template expansion.
+# Input file and salt are unused, but required.
+npx . example/example.html test \
+    --no-embed \
+    --file-template lib/index_template.html \
+    --output index.html \
+    --salt b93bbaf35459951c47721d1f3eaeb5b9 \
